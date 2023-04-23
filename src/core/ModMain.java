d73d7c5e87283794f9883ce7d7cb93a3b526355a
ApsZoldat <apszoldat@gmail.com>
2023-04-23 21:28:33 +0300

    Make you don't crash your game

--- src/core/ModMain.java
+++ src/core/ModMain.java
@@ -13,7 +13,7 @@ public class ModMain extends Mod {
     public ModMain() {
         Events.on(ClientLoadEvent.class, e -> {
             try {
-                ui.settings.graphics.sliderPref("pixelatemultiper", 50, 1, 200, 1,
+                ui.settings.graphics.sliderPref("pixelatemultiper", 50, 5, 200, 1,
                 i -> Float.toString(Math.round((i / 50f) * 100f) / 100f) + "x");
 
                 Reflect.set(Renderer.class, renderer, "pixelator", new OPixelator());
